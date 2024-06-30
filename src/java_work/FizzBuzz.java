53a5c942dda198d54884effd5f18c4125e33796b
chiharu <sharpay_chiharu@yahoo.co.jp>
2024-06-30 10:43:41 +0900

    FizzBuzz作成

--- src/java_work/FizzBuzz.java
+++ src/java_work/FizzBuzz.java
@@ -3,8 +3,52 @@ package java_work;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		if (args.length == 0) {
+			return;
+		}
+		
+		///受け取った引数を数値に変換する 34
+		int max = Integer.parseInt(args[0]);
+		
+		String result = null;
+		
+		///定型分で繰り返し
+		for (int i = 0; i < max; i++) {
+			int num = i + 1;
+			result = "";
+			if (num % 3 == 0) {
+				result = result + "Fizz";
+			}
+			if (num % 5 == 0) {
+				result = result + "Buzz";
+			}
+			if (result.length() == 0) {
+				result = result + num;
+			}
+			System.out.print(result + " ");
+		}
+		
+		
+		//   youyuugaarutokini dousitekounatteirukamitemoraitai
+		for (int i = 0; i < max; i++) {
+			int num = i + 1;
+			if (num % 15 == 0) {
+				System.out.println("FizzBuzz");
+				return ;
+			}
+			
+			if (num % 5 == 0) {
+				System.out.println("Buzz");
+				return;
+			}
+			
+			if (num % 3 == 0) {
+				System.out.println("Fizz");
+				return;
+			}
+			
+			System.out.println(num);
+		}
 	}
 
 }
